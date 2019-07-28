b1264d78528b16ad281202d80b474f848701e3f2
n-vds <94371345+n-vds@users.noreply.github.com>
2019-07-28 13:34:08 +0200

    changes text input

--- app/src/main/java/com/accountant/accountant/MainActivity.java
+++ app/src/main/java/com/accountant/accountant/MainActivity.java
@@ -2,9 +2,11 @@ package com.accountant.accountant;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.accountant.accountant.db.Database;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
             R.id.button3, R.id.button4, R.id.button5,
             R.id.button6, R.id.button7, R.id.button8, R.id.button9
     };
-
-    private String inputString;
 
     private EditText vInput;
     private Button[] inputButtons;
@@ -39,27 +39,29 @@ public class MainActivity extends AppCompatActivity {
         vInput = findViewById(R.id.editText);
         vInput.setShowSoftInputOnFocus(false);
 
-        inputString = "";
-
         db = new Database(this);
     }
 
 
     private void onClick(View which) {
+        Editable text = vInput.getText();
+        CharSequence numeric = text.subSequence(0, text.length() - 2);
+
         for (int i = 0; i < R_BUTTONS.length; i++) {
             if (inputButtons[i] == which) {
-                inputString += String.valueOf(i);
+                if (numeric.equals("0") && i == 0) {
+                    break;
+                }
+                vInput.setText(numeric + " €");
+                break;
             }
         }
 
-        if (which == buttonDot) {
-            inputString = inputString.replaceFirst("\\.", "");
-            inputString += ".";
-        } else if (which == buttonGo) {
-            //TODO
+        if (which == buttonGo) {
+            db.insert(Integer.parseInt(numeric.toString()));
+            vInput.setText("0 €");
+            Toast.makeText(this, "Inserted amount", Toast.LENGTH_SHORT).show();
         }
-
-        vInput.setText(inputString);
     }
 
     @Override
