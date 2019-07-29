2ec6e2a4fc56b7196eea0208d4639474ea8eac94
n-vds <94371345+n-vds@users.noreply.github.com>
2019-07-29 12:46:26 +0200

    adds basic bottom navigation functionality

--- app/src/main/java/com/accountant/accountant/MainActivity.java
+++ app/src/main/java/com/accountant/accountant/MainActivity.java
@@ -2,13 +2,16 @@ package com.accountant.accountant;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import com.accountant.accountant.db.Database;
 
 public class MainActivity extends AppCompatActivity {
     private Database db;
+
+    private BottomNavigationView navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Fragment inputFragment = new InputFragment();
+        navbar = findViewById(R.id.navigation);
+        navbar.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        navbar.setSelectedItemId(R.id.action_insert);
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.content, inputFragment)
+                .replace(R.id.content, new InputFragment())
                 .commit();
     }
 
@@ -29,12 +35,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void switchToData() {
+        navbar.setSelectedItemId(R.id.action_show_list);
+    }
+
+    private void switchDirectlyToData() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.content, new DataListFragment())
-                .addToBackStack(null)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit();
+    }
+
+    private void switchDirectlyToInsert() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, new InputFragment())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
+    }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                break;
+            case R.id.action_insert:
+                switchDirectlyToInsert();
+                break;
+            case R.id.action_show_list:
+                switchDirectlyToData();
+                break;
+        }
+
+        return true;
     }
 
     @Override
