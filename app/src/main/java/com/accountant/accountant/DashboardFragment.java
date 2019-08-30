e837976ba2cb16e63f024bb15cd8e7a4fc890ad5
n-vds <94371345+n-vds@users.noreply.github.com>
2019-08-30 17:58:26 +0200

    better display of average monthly expenses

--- app/src/main/java/com/accountant/accountant/DashboardFragment.java
+++ app/src/main/java/com/accountant/accountant/DashboardFragment.java
@@ -1,73 +1,40 @@
 package com.accountant.accountant;
 
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.accountant.accountant.db.Database;
 import com.accountant.accountant.db.StatisticsEntity;
-import org.w3c.dom.Text;
 
 public class DashboardFragment extends Fragment {
     private static final String[] MONTH_NAMES = new String[]{
             "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"};
-    private static final int[] MONTH_IDS = new int[]{
-            R.id.january, R.id.february, R.id.march, R.id.april, R.id.may, R.id.june,
-            R.id.july, R.id.august, R.id.september, R.id.october, R.id.november, R.id.december
-    };
+
     private TextView tvThisMonth;
     private TextView tvLastThirtyDays;
     private TextView tvMonthlyAvg;
+
+    private RecyclerView vMonthData;
+    private MyAdapter monthDataAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        ConstraintLayout cl = root.findViewById(R.id.root);
-        ConstraintSet constraints = new ConstraintSet();
-
-        ViewGroup.LayoutParams params =
-                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        int[] tvNamesIds = new int[12];
-
-        for (int month = 0; month < 12; month++) {
-            TextView tvName = new TextView(requireContext());
-            tvName.setId(View.generateViewId());
-            tvName.setText(MONTH_NAMES[month] + ":");
-            tvName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-            cl.addView(tvName);
-            tvNamesIds[month] = tvName.getId();
-
-            TextView tvData = new TextView(requireContext());
-            tvData.setId(MONTH_IDS[month]);
-            tvData.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-            cl.addView(tvData);
-        }
-
-        constraints.clone(cl);
-
-        for (int month = 0; month < 12; month++) {
-            constraints.connect(tvNamesIds[month], ConstraintSet.START, R.id.tvMonthlyAvg, ConstraintSet.START);
-            constraints.connect(MONTH_IDS[month], ConstraintSet.START, tvNamesIds[month], ConstraintSet.END);
-            if (month == 0) {
-                constraints.connect(tvNamesIds[month], ConstraintSet.TOP, R.id.tvMonthlyAvg, ConstraintSet.BOTTOM);
-                constraints.connect(MONTH_IDS[month], ConstraintSet.TOP, R.id.dataMonthlyAvg, ConstraintSet.BOTTOM);
-            } else {
-                constraints.connect(tvNamesIds[month], ConstraintSet.TOP, MONTH_IDS[month - 1], ConstraintSet.BOTTOM);
-                constraints.connect(MONTH_IDS[month], ConstraintSet.TOP, MONTH_IDS[month - 1], ConstraintSet.BOTTOM);
-            }
-        }
-        constraints.applyTo(cl);
+        vMonthData = root.findViewById(R.id.listMonthData);
+        vMonthData.setHasFixedSize(true);
+        vMonthData.setLayoutManager(new LinearLayoutManager(requireContext()));
+        monthDataAdapter = new MyAdapter();
+        vMonthData.setAdapter(monthDataAdapter);
 
         return root;
     }
@@ -84,17 +51,52 @@ public class DashboardFragment extends Fragment {
         super.onResume();
         Database db = ((MainActivity) requireActivity()).getDatabase();
         StatisticsEntity stats = db.queryStatistics();
-        tvThisMonth.setText(stats.spentThisMonth / 100f + " €");
-        tvLastThirtyDays.setText(stats.spentLastThirtyDays / 100f + " €");
-        tvMonthlyAvg.setText(stats.spendingAvgMonth / 100f + " €");
-        for (int month = 0; month < 12; month++) {
-            TextView tv = getView().findViewById(MONTH_IDS[month]);
-            float avg = stats.spendingAvgMonthByMonth[month];
-            if (Float.isNaN(avg)) {
-                tv.setText("?");
+        tvThisMonth.setText(Math.round(stats.spentThisMonth / 100f) + " €");
+        tvLastThirtyDays.setText(Math.round(stats.spentLastThirtyDays / 100f) + " €");
+        tvMonthlyAvg.setText(Math.round(stats.spendingAvgMonth / 100f) + " €");
+        monthDataAdapter.setData(stats.spendingAvgMonthByMonth);
+        monthDataAdapter.notifyDataSetChanged();
+    }
+
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private float[] data = null;
+
+        void setData(float[] data) {
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MyViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.dashboard_month_spent_row, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            holder.monthName.setText(MONTH_NAMES[position]);
+            if (data == null || Float.isNaN(data[position])) {
+                holder.amount.setText("?");
             } else {
-                tv.setText(avg / 100f + " €");
+                holder.amount.setText(String.format("%d €", Math.round(data[position] / 100f)));
             }
         }
+
+        @Override
+        public int getItemCount() {
+            return 12;
+        }
+
+        private static class MyViewHolder extends RecyclerView.ViewHolder {
+            private TextView monthName;
+            private TextView amount;
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+                monthName = itemView.findViewById(R.id.monthName);
+                amount = itemView.findViewById(R.id.tvAmount);
+            }
+        }
+
     }
 }
